5670a9d854ec898ad2f0011ef779c9d6f46ba936
Marten Payne <martenp@amazon.com>
2017-11-24 00:51:13 +0000

    Found some leaks in mosca
     - cleaned up issues with lambda connecting
     - diagnostics
       - need to run with node --expose-gc

--- producer.js
+++ producer.js
@@ -4,22 +4,23 @@ const DEBUG = true                      // turn on console logging
 const FREQ = 1                          // Hz
 const MQTT_TOPIC = "/flight/data"       // topic on to which to publish
 
-
 // -------------------------- DO NOT EDIT BELOW THIS LINE --------------------------
 
-
+var memwatch = require('memwatch-next');
 var mosca = require('mosca');
 var fs = require('fs');
 var stream = require('stream');
 var lineByLine = require('n-readlines');
 var countLinesInFile = require('./lib/countLinesInFile')
 
-
 var dataFilename = "data.csv"
 var instream = fs.createReadStream(dataFilename);
 var numberOfLines = 0 ;
 var INTERVAL = 1000/FREQ                // ms
 
+memwatch.on('leak', (info) => {
+    console.error('memory leak detected:\n', info);
+});
 
 var settings = {
     port: 1883,
@@ -29,9 +30,12 @@ var settings = {
     }
 }
 
+var publishClientID = 'IoT_GGD'; // ID of treadstone client
 var lineCounter = 1;
+var liner = new lineByLine(dataFilename);
 
 var server = new mosca.Server(settings);
+
 server.on('clientConnected', clientConnected);
 
 server.on('ready', setup);
@@ -42,59 +46,72 @@ server.on('published', function(packet, client) {
 
 // fired when the mqtt server is ready
 function setup() {
-  if (DEBUG)
-    {
-        console.log('Mosca server is up and running');
-        console.log(`Publish rate: ${FREQ} Hz`)
-    }
+  if (DEBUG) {
+      console.log('Mosca server is up and running');
+      console.log(`Publish rate: ${FREQ} Hz`)
+  }
 
   countLinesInFile(dataFilename, (err, numLines) => {
-   if (!err){
-       numberOfLines = numLines
-   }
-    });
+      if (!err) {
+	  numberOfLines = numLines
+      }
+  });
 }
 
 server.on('clientDisconnected', function(client) {
-    if (DEBUG)
-        console.log('Client Disconnected:', client.id);
+    if (DEBUG) {
+        console.log('client disconnected:', client.id);
+    }
 }); 
 
-
-
 function clientConnected(client) {
-    if (DEBUG)
-        console.log('client connected', client.id);
-
-    var liner = new lineByLine(dataFilename);
+    global.gc();
+    
+    // only start the publish timer if the client is treadstone
+    if (client.id.trim().startsWith(publishClientID.trim())) {
+	console.log('TS client connected');
+	console.log('starting interval publishing');
+    }
+    else {
+	if (DEBUG) {
+	    console.log('non TS client connected');
+	}
+    }
+    
     var lineNumber = 0 ;
         
-    var interval = setInterval(function(){
-
+    var interval = setInterval(function() {
         var line = liner.next();  
         var jsonLine = JSON.parse(line).data
-
+	line = null;
+	
         var message = {
             topic: MQTT_TOPIC,
             payload : JSON.stringify(jsonLine),
             qos: 0, // 0, 1, or 2
             retain: false // or true
-        }
+        };
 
 
         server.publish(message, function() {
-           
+	    message = null;
+	    jsonLine = null;
         });
 
-        if (numberOfLines-lineNumber > 0){
+        if (numberOfLines-lineNumber > 0) {
             lineNumber++;
-        } else {
-            lineNumber=0;
-            liner = new lineByLine(dataFilename);
         }
+	else {
+            lineNumber = 0;
+	    liner = null;
 
-        
-    }.bind(this),INTERVAL)
+	    if (DEBUG) {
+		console.log("reloading data");
+	    }
+	    
+	    liner = new lineByLine(dataFilename);
+        }
+    }.bind(this), INTERVAL)
 }
 
 
